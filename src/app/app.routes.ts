00949896a1f211b42b1be541755dd3fc2ed3e643
Bourikas <bourikas.georgios@gmail.com>
2024-04-01 19:08:09 +0300

    ok

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,13 @@
 import { Routes } from '@angular/router';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { EventBindExampleComponent } from './components/event-bind-example/event-bind-example.component';
+import { ForDirectiveExampleComponent } from './components/for-directive-example/for-directive-example.component';
+import { ComponentInputExampleComponent } from './components/component-input-example/component-input-example.component';
 
 export const routes: Routes = [
-    {path: 'event-bind-example'}
+    {path: 'event-bind-example', component: EventBindExampleComponent},
+    {path: 'welcome', component: WelcomeComponent},
+    {path: 'for-directive-example', component:ForDirectiveExampleComponent},
+    {path: 'component-input-example', component:ComponentInputExampleComponent},
+    {path: "", redirectTo: '/welcome', pathMatch: 'full'}
 ];
