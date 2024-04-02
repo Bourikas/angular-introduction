d733524768df5f802222820b7dabdfc224204ad5
Bourikas <bourikas.georgios@gmail.com>
2024-04-03 00:17:01 +0300

    step - 9

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { EventBindExampleComponent } from './components/event-bind-example/event-bind-example.component';
 import { ForDirectiveExampleComponent } from './components/for-directive-example/for-directive-example.component';
-import { ComponentInputExampleComponent } from './components/component-input-example/component-input-example.component';
+import { ComponentOutputExampleComponent } from './components/component-output-example/component-output-example.component';
 import { SimpleDatatableComponent } from './components/simple-datatable/simple-datatable.component';
 import { SimpleDatatableExampleComponent } from './components/simple-datatable-example/simple-datatable-example.component';
 
@@ -10,8 +10,9 @@ export const routes: Routes = [
     {path: 'event-bind-example', component: EventBindExampleComponent},
     {path: 'welcome', component: WelcomeComponent},
     {path: 'for-directive-example', component:ForDirectiveExampleComponent},
-    {path: 'component-input-example', component:ComponentInputExampleComponent},
+    {path: 'component-output-example', component:ComponentOutputExampleComponent},
     {path: "", redirectTo: '/welcome', pathMatch: 'full'},
     // {path: "simple-datatable", component:SimpleDatatableComponent},
+    {path: "component-output-example", component:ComponentOutputExampleComponent},
     {path:"simple-datatable-example", component:SimpleDatatableExampleComponent}
 ];
