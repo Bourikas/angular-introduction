be18e5f66d1316aa4ba4288abe853a04c2382b2f
Bourikas <bourikas.georgios@gmail.com>
2024-04-08 19:59:56 +0300

    οκ

---
+++ src/app/components/eperson-template-driven-form-example/eperson-template-driven-form-example.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { EpersonTemplateDrivenFormComponent } from '../eperson-template-driven-form/eperson-template-driven-form.component';
+
+@Component({
+  selector: 'app-eperson-template-driven-form-example',
+  standalone: true,
+  imports: [EpersonTemplateDrivenFormComponent],
+  templateUrl: './eperson-template-driven-form-example.component.html',
+  styleUrl: './eperson-template-driven-form-example.component.css'
+})
+export class EpersonTemplateDrivenFormExampleComponent {
+
+}
